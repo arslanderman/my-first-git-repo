75f75e0b0ada69f362b1505e22499078dee89c4a
Arslan Derman <arslandermanson@gmail.com>
2022-07-16 21:47:03 +0200

    third commit

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -2,6 +2,8 @@ public class MyGitClass {
     public static void main(String[] args) {
 
         System.out.println("i am learning guthub!!");
+        System.out.println("i am adding new code...");
+        System.out.println("I am adding one more");
     }
 
 }
